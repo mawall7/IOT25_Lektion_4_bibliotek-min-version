fbc8342159c82ec2a8a8be5ca04c5d9d297f1d0d
MATTIAS_WALLERMAN <wallermanmattias@yahoo.se>
2025-10-30 10:47:13 +0100

    add comments

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_BOOKS 50
+#define MAX_BOOKS 50  //kommentar// hur används define och inte const? svar: är ett makro och har ej en typ utan ersätts med en text där namnet står och sker innan kompilering, kan vara svårare att debugga, men, sparar minne eftersom inget minne allokeras för en typ.
 #define MAX_TITLE_LENGTH 100
 #define MAX_LIBRARY_NAME 50
-
+//kommentar//obs man kan även skapa struct med struct Book { char title, int pages.. }, utan typdef
+//men med typedef blir det enklare att skapa den istället för struct Book myBook = {...} kan man 
+//nu skriva Book myBook = {..}
 // ---------------- STRUCT DEFINITIONS ----------------
 typedef struct {
     char title[MAX_TITLE_LENGTH];
@@ -20,21 +22,22 @@ typedef struct {
 } Library;
 
 // ---------------- FUNCTION DECLARATIONS ----------------
-void add_book(Library *lib, const char *title, int pages);
+void add_book(Library *lib, const char *title, int pages); //hade inte behövts här eftersom vi även har definitioner, men hade använts av en header fil.
 void print_books(const Library *lib);
 void search_book(const Library *lib, const char *title);
 void borrow_book(Library *lib, const char *title);
 void return_book(Library *lib, const char *title);
 
 // ---------------- FUNCTION IMPLEMENTATIONS ----------------
+//kommentar// -> Är samma som (*lib) för att komma åt det lådan som pekaren pekar på  
 void add_book(Library *lib, const char *title, int pages) {
-    if (lib->num_books >= MAX_BOOKS) {
+    if (lib->num_books >= MAX_BOOKS) {  
         printf("The library is full!\n");
         return;
     }
-
+    //kommentar// strncpy används för att tilldela en char array en ny sträng, i c inte tillåtet att tilldela en array en ny sträng efter deklarationen av arrayen, då man kan skriva t.ex. char myStr[5] = "hejsan", men strncpy för att ändra på värdet efteråt.   
     Book new_book;
-    strncpy(new_book.title, title, MAX_TITLE_LENGTH);
+    strncpy(new_book.title, title, MAX_TITLE_LENGTH); 
     new_book.pages = pages;
     new_book.is_borrowed = false;
 
